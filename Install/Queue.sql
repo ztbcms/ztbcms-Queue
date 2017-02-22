4ffc2b6df6305e5a751065dd3c853fbd5d822f64
Jayin Ton <tonjayin@gmail.com>
2017-02-23 01:37:28 +0800

    sql: add name

--- Install/Queue.sql
+++ Install/Queue.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS `cms_queue_job`;
 CREATE TABLE `cms_queue_job` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(64) NOT NULL COMMENT '任务类名',
   `queue` varchar(255) NOT NULL DEFAULT '' COMMENT '队列名称',
   `payload` text NOT NULL COMMENT '数据',
   `attempts` tinyint(3) NOT NULL DEFAULT '0' COMMENT '重试次数',
