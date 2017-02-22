7a3d9039b82bc8cbe1f8e0a968fed3939cb234b1
Jayin Ton <tonjayin@gmail.com>
2017-02-23 00:50:04 +0800

    update sql

--- Install/Queue.sql
+++ Install/Queue.sql
@@ -4,7 +4,8 @@ CREATE TABLE `cms_queue_job` (
   `queue` varchar(255) NOT NULL DEFAULT '' COMMENT '队列名称',
   `payload` text NOT NULL COMMENT '数据',
   `attempts` tinyint(3) NOT NULL DEFAULT '0' COMMENT '重试次数',
-  `reserved_at` int(11) NOT NULL COMMENT '保留时间',
+  `reserved_at` int(11) NOT NULL COMMENT '取出时间',
   `available_at` int(11) NOT NULL COMMENT '可用时间',
+  `status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '任务状态：0排队中,1工作中,2已完成',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='队列任务';
