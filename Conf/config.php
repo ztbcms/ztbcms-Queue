e75877c4699aadd569a1427bf904b8619aa3b815
Jayin Ton <tonjayin@gmail.com>
2017-02-23 10:32:31 +0800

    sleep time config in `config.php`

--- Conf/config.php
+++ Conf/config.php
@@ -5,5 +5,6 @@
  */
 
 return array(
-    'QUEUE_TYPE' => 'database' ,//队列类型
+    'QUEUE_TYPE' => 'database',//队列类型
+    'QUEUE_SLEEP' => '10',//队列空闲时，休眠时间
 );
--- Controller/WorkerController.class.php
+++ Controller/WorkerController.class.php
@@ -8,6 +8,7 @@ namespace Queue\Controller;
 
 use Queue\Libs\Queue;
 use Queue\Libs\Worker;
+use Queue\Libs\WorkerOptions;
 
 class WorkerController extends QueueController {
 
@@ -16,11 +17,12 @@ class WorkerController extends QueueController {
      * 入口
      */
     public function run() {
-        //指定开启的队列
+        //指定开启的队列,从左到右，优先级别由高到低
         $queue = I('get.queue', '');
 
         $queue_manager = Queue::getInstance();
         $worker = new Worker($queue_manager);
-        $worker->run($queue);
+        $workerOptions = new WorkerOptions(C('QUEUE_SLEEP'));
+        $worker->run($queue, $workerOptions);
     }
 }
