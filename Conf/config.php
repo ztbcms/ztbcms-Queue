4663d45523d7c9dc90024104fd7bc0823b33b9c1
Jayin Ton <tonjayin@gmail.com>
2017-02-26 23:10:16 +0800

    队列启动加入私钥校验

--- Conf/config.php
+++ Conf/config.php
@@ -5,6 +5,7 @@
  */
 
 return array(
+    'QUEUE_SECRET_KEY' => '',//队列校验私钥
     'QUEUE_TYPE' => 'database',//队列类型
     'QUEUE_SLEEP' => '5',//队列空闲时，休眠时间
     'QUEUE_MAX_RETRY' => 3, //最大重试次数
--- Controller/QueueController.class.php
+++ Controller/QueueController.class.php
@@ -10,4 +10,16 @@ use Common\Controller\Base;
 
 class QueueController extends Base {
 
+
+    protected function _initialize() {
+        parent::_initialize();
+        //私钥校验
+        $queue_secret_key = I('get._qsk', '');
+        if ($queue_secret_key != C('QUEUE_SECRET_KEY')) {
+            echo '私钥不匹配';
+            exit();
+        }
+    }
+
+
 }
