652229befd49ecf49faec9d9d4ac1dcce29a5275
Jayin Ton <tonjayin@gmail.com>
2017-05-21 23:30:59 +0800

    队列空闲时，休眠时间默认为3秒

--- Conf/config.php
+++ Conf/config.php
@@ -7,6 +7,6 @@
 return array(
     'QUEUE_SECRET_KEY' => '',//队列校验私钥
     'QUEUE_TYPE' => 'database',//队列类型
-    'QUEUE_SLEEP' => '5',//队列空闲时，休眠时间
+    'QUEUE_SLEEP' => '3',//队列空闲时，休眠时间
     'QUEUE_MAX_RETRY' => 3, //最大重试次数
 );
