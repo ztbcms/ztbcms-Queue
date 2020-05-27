5f9779ebb5bb89509bca5379cba4a5532067512a
Jayin Ton <tonjayin@gmail.com>
2020-05-27 16:41:44 +0800

    补充sql

--- Install/Queue.sql
+++ Install/Queue.sql
@@ -12,4 +12,22 @@ CREATE TABLE `cms_queue_job` (
   `result` tinyint(2) NOT NULL DEFAULT '0' COMMENT '执行结果:0无,1正常 2异常',
   `create_time` bigint(13) unsigned NOT NULL COMMENT '任务创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='队列任务';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='队列任务';
+
+
+CREATE TABLE `cms_queue_faild_job` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(64) CHARACTER SET utf8 NOT NULL COMMENT '任务类名',
+  `queue` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '' COMMENT '队列名称',
+  `payload` text CHARACTER SET utf8 NOT NULL COMMENT '数据',
+  `attempts` tinyint(3) NOT NULL DEFAULT '0' COMMENT '重试次数',
+  `reserved_at` bigint(13) unsigned NOT NULL COMMENT '取出时间',
+  `available_at` bigint(13) unsigned NOT NULL COMMENT '可用时间',
+  `status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '任务状态：0排队中,1工作中,2已完成',
+  `start_time` bigint(13) unsigned NOT NULL COMMENT '任务执行开始时间',
+  `end_time` bigint(13) unsigned NOT NULL COMMENT '任务执行结束时间',
+  `result` tinyint(2) NOT NULL DEFAULT '0' COMMENT '执行结果:0无,1正常 2异常',
+  `create_time` bigint(13) unsigned NOT NULL COMMENT '任务创建时间',
+  `exception` longtext CHARACTER SET utf8 COMMENT '异常',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='失败任务';
--- Uninstall/Queue.sql
+++ Uninstall/Queue.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS `cms_queue_job`;
+DROP TABLE IF EXISTS `cms_queue_faild_job`;
